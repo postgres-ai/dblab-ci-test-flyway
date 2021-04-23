4dcdb1100ef0925ece9041e421ce7738fc67a9e5
Vitaliy Kukharik <vitabaks@gmail.com>
2021-04-23 15:26:28 +0300

    Update initial.sql

--- deploy/initial.sql
+++ deploy/initial.sql
@@ -1,12 +1,5 @@
 -- deploy runci:initial to pg
 
-
-begin;
-
 create table abc (a int);
-
-dddd; -- try error
-
-commit;
 
 create index pgbench_accounts_no_concurrently_idx on pgbench_accounts (bid);
